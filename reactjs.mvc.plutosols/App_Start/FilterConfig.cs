c91f0cd308d1d52adc98bb463cea87342a6b3490
Kajornjit Songsaen <kajornjit@gmail.com>
2017-11-19 14:33:16 +0700

    Add more projects

---
+++ reactjs.mvc.plutosols/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace reactjs.mvc.plutosols
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
